8cb63a55a2b68263c5bbb793a772b9084bb2e323
Nighaar Taneem <tnighaar@gmail.com>
2026-04-01 19:07:38 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
