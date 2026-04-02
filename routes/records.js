24b4dd098c70c7a030a6d3904a7b3067548ee724
Nighaar Taneem <tnighaar@gmail.com>
2026-04-02 21:46:30 +0530

    Role-based access control added

--- routes/records.js
+++ routes/records.js
@@ -1,21 +1,64 @@
 const express = require("express");
 const router = express.Router();
+
 const authMiddleware = require("../middleware/authMiddleware");
-const { getSummary } = require("../controllers/recordController");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
   createRecord,
   getRecords,
   updateRecord,
-  deleteRecord
+  deleteRecord,
+  getSummary
 } = require("../controllers/recordController");
 
-// Protected routes
-router.post("/", authMiddleware, createRecord);
-router.get("/summary", authMiddleware, getSummary);
-router.get("/", authMiddleware, getRecords);
-router.put("/:id", authMiddleware, updateRecord);
-router.delete("/:id", authMiddleware, deleteRecord);
+// Summary (Analyst + Admin)
+router.get("/summary", authMiddleware, roleMiddleware("analyst", "admin"), getSummary);
 
+// GET (Viewer + above)
+router.get("/", authMiddleware, roleMiddleware("viewer", "analyst", "admin"), getRecords);
+
+// CREATE (Admin only)
+router.post("/", authMiddleware, roleMiddleware("admin"), createRecord);
+
+// UPDATE (Admin only)
+router.put("/:id", authMiddleware, roleMiddleware("admin"), updateRecord);
+
+// DELETE (Admin only)
+router.delete("/:id", authMiddleware, roleMiddleware("admin"), deleteRecord);
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// const authMiddleware = require("../middleware/authMiddleware");
+// const { getSummary } = require("../controllers/recordController");
+
+// const {
+//   createRecord,
+//   getRecords,
+//   updateRecord,
+//   deleteRecord
+// } = require("../controllers/recordController");
+
+// // Protected routes
+// router.post("/", authMiddleware, createRecord);
+// router.get("/summary", authMiddleware, getSummary);
+// router.get("/", authMiddleware, getRecords);
+// router.put("/:id", authMiddleware, updateRecord);
+// router.delete("/:id", authMiddleware, deleteRecord);
+
+
+// module.exports = router;
