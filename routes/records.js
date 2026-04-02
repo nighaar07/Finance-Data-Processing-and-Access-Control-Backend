2bd9420f949d9b6ebe065d1375eace8e701c788a
Nighaar Taneem <tnighaar@gmail.com>
2026-04-02 22:28:10 +0530

    Final submission of Finance Data Processing and Access Control Backend

--- routes/records.js
+++ routes/records.js
@@ -28,37 +28,3 @@ router.put("/:id", authMiddleware, roleMiddleware("admin"), updateRecord);
 router.delete("/:id", authMiddleware, roleMiddleware("admin"), deleteRecord);
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const router = express.Router();
-// const authMiddleware = require("../middleware/authMiddleware");
-// const { getSummary } = require("../controllers/recordController");
-
-// const {
-//   createRecord,
-//   getRecords,
-//   updateRecord,
-//   deleteRecord
-// } = require("../controllers/recordController");
-
-// // Protected routes
-// router.post("/", authMiddleware, createRecord);
-// router.get("/summary", authMiddleware, getSummary);
-// router.get("/", authMiddleware, getRecords);
-// router.put("/:id", authMiddleware, updateRecord);
-// router.delete("/:id", authMiddleware, deleteRecord);
-
-
-// module.exports = router;
